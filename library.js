5ceefe441b7e19a330b9f45ea8552f7d527e3c0e
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-12-11 20:17:58 +0200

    Initial commit

---
+++ library.js
@@ -0,0 +1,47 @@
+let myLibrary = [
+    {
+        title: "Robinson Crusoe",
+        author: "Daniel Defoe",
+        pages: 200,
+        read: true,
+    }
+];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayLibrary() {
+    const library = document.querySelector("#library");
+
+    myLibrary.forEach(book => {
+        const bookElem = document.createElement("tr");
+
+        const title = document.createElement("td");
+        title.innerText = book.title;
+        bookElem.appendChild(title);
+
+        const author = document.createElement("td");
+        author.innerText = book.author;
+        bookElem.appendChild(author);
+
+        const pages = document.createElement("td");
+        pages.innerText = book.pages;
+        bookElem.appendChild(pages);
+
+        const read = document.createElement("td");
+        read.innerText = book.read ? "Read" : "Not read";
+        bookElem.appendChild(read);
+
+        library.appendChild(bookElem);
+    });
+}
+
+displayLibrary();
